980edf6e93a194723f989eb08eb6debb0d66c391
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-09-26 18:02:40 +0530

    Create mpk-4.py

---
+++ mpk-4.py
@@ -0,0 +1,11 @@
+quicksort(a,0,arr_size-1)
+   1=0
+   r=arr_size-1
+   while 1<r:
+      if (a[1]+a[r]==sum):
+          return 1
+      elif(a[1]+a[r]<sum) :
+           1+=1
+       else:
+           return 0
+    
