41e3f68187542403c9f454778d8a65ddda710b73
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-09-26 17:39:42 +0530

    Update and rename mpk-3.py to mpkk-3.py

--- mpkk-3.py
+++ mpkk-3.py
@@ -2,6 +2,5 @@ a=[]
 n=int(input("enter number of element:"))
 for i in range(1,n-1):
     b=int(input("enter smallest  element:"))
-    a.append(b)
-a.sort() 
+    a.append(b) 
 print("smallest element is:",a[n-1])   
