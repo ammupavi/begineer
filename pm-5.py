b8be8175636382d3af045fd36fa4ea8dea80a6a1
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-09-26 15:17:59 +0530

    Create pm-5.py

---
+++ pm-5.py
@@ -0,0 +1,10 @@
+lower=800
+upper=2000
+print("even number between ",lower,"and",upper,"are:")
+for num in range(lower,upper+1):
+    if num>1:
+        for i in range(2,num):
+            if (num%i)==0:
+                break
+            else:
+                print(num)
