df7b3b360153f8b70158f7e8aaf2e9a9d2825542
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-09-27 17:39:49 +0530

    Create sp-3.py

---
+++ sp-3.py
@@ -0,0 +1,11 @@
+fname=input("enter file name:")
+k=0
+with open(fname,'r') as f:
+    for line in f:
+        words=line.split()
+        for i in words:
+            for letter in i:
+                if(letter.isspace):
+                    k=k+1
+print("occurances of blank spaces:")
+print(k)
