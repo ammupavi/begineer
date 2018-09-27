f33919eb7dd834aaf9eb5aa06106e9238dfa7016
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-09-27 17:18:47 +0530

    Create sp-1.py

---
+++ sp-1.py
@@ -0,0 +1,10 @@
+string=raw_input("enter string:")
+char=0
+word=1
+for i in string:
+    char=char+1
+    if(i==''):
+        word=word+1
+print("number of words in the string:") 
+print(word)
+print("number of character in the string:")
