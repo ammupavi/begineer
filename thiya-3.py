05ff459fbcf1ac5294e5bce0faf251f922a4bd24
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-10-01 17:29:36 +0530

    Create thiya-3.py

---
+++ thiya-3.py
@@ -0,0 +1,7 @@
+list=[]
+n=int(input("how many numbers:")
+for n in range(num):
+  n=int(input("enter any number:")
+  list.append numbers(numbers)
+print("maximun element in the list is :",max(list),"\n maximun element in the list"
+  
