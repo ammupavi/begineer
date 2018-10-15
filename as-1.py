0adcae8ce39bd138bd31b033b97f62d2a94bec11
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-10-15 17:19:28 +0530

    Create as-1.py

---
+++ as-1.py
@@ -0,0 +1,6 @@
+length=float(input("enter the length of a cubiod"))
+width=float(input("enter the width a cuboid"))
+height=float(input("enter the height of a cuboid"))
+print("surface area of cubiod")
+print(the volumn of cubiod')
+print("the surface area')
