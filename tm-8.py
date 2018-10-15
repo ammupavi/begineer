e4096589e36b84daef2039b614e5cd6c49cafdd7
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-10-15 17:08:47 +0530

    Create tm-8.py

---
+++ tm-8.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+  if (a>b):
+    min 1=a
+      else:
+         min 1=b
+       while(1)
+         print("lcm is min)
+         min 1=min 1+1
