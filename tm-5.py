a2b084166aced1505c1a46f5daf84cb87660e7fd
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-10-15 16:57:19 +0530

    Create tm-5.py

---
+++ tm-5.py
@@ -0,0 +1,7 @@
+input_string=input("enter a string")
+ output_string=' ':
+ if(odd or even==1):
+   print("string on odd position")
+    elif(odd or even==2):
+    print("string or even position")
+    
