9df995057540701eeef391b98d8284d0cf5d154f
ammupavi <40994683+ammupavi@users.noreply.github.com>
2018-10-15 17:22:41 +0530

    Create as-2.py

---
+++ as-2.py
@@ -0,0 +1,10 @@
+num=int(input("enter the value"))
+hold=num
+sum=0
+if(num <=0):
+  print("enter positive numbers")
+ else:
+   while(num>0):
+    sum=sum+num
+    num=num-1:
+    print("the natural numbers")
